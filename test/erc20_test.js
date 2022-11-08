53b2682fde3ad7d8c5d508eee5085346fce5deb1
Hulk Chen <>
2022-11-08 21:41:13 +0800

    找到兩個有uni / usdc帳號轉錢給singer0\1

--- test/erc20_test.js
+++ test/erc20_test.js
@@ -293,5 +293,136 @@ describe('CToken', function () {
 
     });
 
+    // 第六題
+    it("six", async () => {
+
+      // 先找到擁有大量 USDC以及UNI的帳號
+      // 有很多UNI
+      let usdcTransferAmount = ethers.utils.parseUnits("50000", 6);
+      let uniTransferAmount = ethers.utils.parseUnits("2000", 18);
+      accounts = await ethers.getSigners();
+      uni = await ethers.getContractAt("EIP20Interface","0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984");
+      usdc = await ethers.getContractAt("EIP20Interface","0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
+
+      const impersonatedSignerUNI = await ethers.getImpersonatedSigner("0x33Ddd548FE3a082d753E5fE721a26E1Ab43e3598");
+      const impersonatedSignerUSDC = await ethers.getImpersonatedSigner("0xAe2D4617c862309A3d75A0fFB358c7a5009c673F");
+
+
+      await uni.connect(impersonatedSignerUNI).transfer(accounts[0].address,uniTransferAmount);
+      let uniBalanceOfSinger1 = await uni.balanceOf(accounts[0].address);
+      console.log("uniBalanceOfSinger1");
+      console.log(uniBalanceOfSinger1);
+
+      await usdc.connect(impersonatedSignerUSDC).transfer(accounts[1].address,usdcTransferAmount);
+      let usdcBalanceOfSinger2 = await usdc.balanceOf(accounts[1].address);
+      console.log("usdcBalanceOfSinger2");
+      console.log(usdcBalanceOfSinger2);
+
+
+
+
+
+      //有很多USDC
+      // 0x8c1A4C98C470900567FB9e764243c89cDa79400C
+      // const impersonatedSigner = await ethers.getImpersonatedSigner("0x1234567890123456789012345678901234567890");
+
+
+      // 轉錢到Singer1 50000顆 USDC、1000顆UNI 以及 Singer2 2000顆UNI
+
+      //  設定comptroller、oracle USDC 1元、 UNI 10元 collateral factor 50%、exchange rate 1
+      //  Singer1 創造cUSDC池 mint 10000顆 cUSDC 以及 Singer2 創造cUNI池 mint 1000顆
+      // Singer2 抵押 1000 UNI 借 5000顆 USDC
+
+      // 將 UNI價格調整為 6.2 ， Singer2 成為被清算人
+      // Signer1 執行AAve flash loan 借USDC後 償還 Singer2借的 2500USDC（假設 Close Factor是 50%）
+      // 償還完後，Signer1 取得 cUNI後，接下來 redeem 回 UNI
+      // Singer1 到uniswap 上 將UNI換成USDC
+      // Singer1 償還 在Aave上面借的USDC + premium
+      // 照理說會有剩餘，這時就完成套利囉
+
+      // const { CERC20Deploy, anotherCERC20Deploy, ERC20Deploy , anotherERC20Deploy,comptrollerDeploy, oracleDeploy} = await loadFixture(deployAllModel);
+
+      // const [owner, ...singer] = await ethers.getSigners();
+
+      // const MINT_AMOUNT = 1000n * DECIMAL;
+    
+      // const COLLATERAL_FACTOR = ethers.utils.parseUnits("0.5", 18);
+  
+      // //第一種ERC20 Token mint
+      // await ERC20Deploy.approve(CERC20Deploy.address,MINT_AMOUNT);
+
+      // await comptrollerDeploy._supportMarket(CERC20Deploy.address);
+      // //由owner先放一些AToken 進池子裡
+      // await CERC20Deploy.mint(MINT_AMOUNT);
+      
+      // //第二種ERC20 Token mint
+      // await anotherERC20Deploy.approve(anotherCERC20Deploy.address,MINT_AMOUNT);
+      // await comptrollerDeploy._supportMarket(anotherCERC20Deploy.address);
+      // //由owner先放一些BToken 進池子裡
+      // await anotherCERC20Deploy.mint(MINT_AMOUNT);
+
+      // // 設定 A Token 價格為 1
+      // await oracleDeploy.setUnderlyingPrice(CERC20Deploy.address,ethers.utils.parseUnits("1", 18))
+      // // 設定 B Token 價格為 100
+      // await oracleDeploy.setUnderlyingPrice(anotherCERC20Deploy.address,ethers.utils.parseUnits("100", 18))
+      // // comptroller要正常運作需要設定 oracle 以及 colleateral factor
+      // await comptrollerDeploy._setPriceOracle(oracleDeploy.address);
+      // await comptrollerDeploy._setCollateralFactor(anotherCERC20Deploy.address, COLLATERAL_FACTOR );
+      // // 接下來要讓 owner 以及 signer1 分別執行 enterMarkets
+      // // 要不然在comptroller 裡面的 accountAssets參數 會找不到 owner / signer1 
+      // // 而導致後續的 borrow 出現 流動性不足錯誤
+      // await comptrollerDeploy.enterMarkets([CERC20Deploy.address,anotherCERC20Deploy.address]);
+      // await comptrollerDeploy.connect(singer[0]).enterMarkets([CERC20Deploy.address,anotherCERC20Deploy.address]);
+      // await comptrollerDeploy.connect(singer[1]).enterMarkets([CERC20Deploy.address,anotherCERC20Deploy.address]);
+
+      // // 由 owner 轉 B Token 給 singer1
+      // await anotherERC20Deploy.transfer(singer[0].address, ethers.utils.parseUnits("1", 18));
+      // // 由 owner 轉 A Token 給 singer2
+      // await ERC20Deploy.transfer(singer[1].address, ethers.utils.parseUnits("200", 18));
+
+      // await anotherERC20Deploy.connect(singer[0]).approve(anotherCERC20Deploy.address, ethers.utils.parseUnits("1", 18));
+      // // singer1 mint cB Token 當作等會要借 A Token 的抵押品
+      // await anotherCERC20Deploy.connect(singer[0]).mint(1n * DECIMAL);
+      // // singer1 借 50 A Token出來
+      // await CERC20Deploy.connect(singer[0]).borrow(ethers.utils.parseUnits("50", 18));
+
+      // // 重設TokenB 價格 從100調整為50
+      // await oracleDeploy.setUnderlyingPrice(anotherCERC20Deploy.address,ethers.utils.parseUnits("50", 18))
+      // // 設定 代償比率
+      // // 預設最低是 5%
+      // // closeFactorMinMantissa = 0.05e18; // 0.05
+      // // 預設最高是 90%
+      // // closeFactorMaxMantissa = 0.9e18; // 0.9
+      // // 設定代償比率為 50%
+      // await comptrollerDeploy._setCloseFactor(ethers.utils.parseUnits("0.5", 18))
+
+      // // 設定清算人的激勵費 10%
+      // // 沒有預設值
+      // await comptrollerDeploy._setLiquidationIncentive(ethers.utils.parseUnits("1.1", 18))
+
+      // await ERC20Deploy.connect(singer[1]).approve(CERC20Deploy.address, ethers.utils.parseUnits("100", 18));
+
+      
+      // // singer2 幫 singer1 還一半
+      // // cTokenColleateral  cB Token --> 清算人決定要哪種 被清算人獎勵
+      // await CERC20Deploy.connect(singer[1]).liquidateBorrow(
+      //   singer[0].address,
+      //   ethers.utils.parseUnits("25", 18),
+      //   anotherCERC20Deploy.address
+      // )
+
+
+      // // user2 原本有 200A 幫 user1還 25A後，應該只剩 175A
+      // const singer2ATokenBalance = await ERC20Deploy.balanceOf(singer[1].address);
+      // expect(singer2ATokenBalance).to.equal(ethers.utils.parseUnits("175", 18));
+
+
+      // // user2 代還款後，會取得0.55個cBToken
+      // const singer2cBTokenBalance = await anotherCERC20Deploy.balanceOf(singer[1].address);
+      // expect(singer2cBTokenBalance).to.equal(ethers.utils.parseUnits("0.55", 18));
+
+
+    });
+
   });
 });
